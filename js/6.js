032f988c4e42d4b20c8c1eb471ffefbf95711464
Lisandro Civili <lisandrocivili13@gmail.com>
2023-11-02 20:58:40 -0300

    ejercicio 6 realizado

---
+++ js/6.js
@@ -0,0 +1,12 @@
+let contador = 0;
+let numerosPiramide = `1`;
+let piramide = ``;
+do {
+    
+    for (let i = 0; i < contador+1; i++) {
+        document.write(`${numerosPiramide}` )
+    }
+    document.write("<br>")
+    numerosPiramide++
+    contador++
+} while (contador <= 30);
